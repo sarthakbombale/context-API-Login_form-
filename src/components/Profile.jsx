e7d790661c35be3bbacf12fb52cc01eb1266c8bb
sarthakbombale <sarthakbomble250@gmail.com>
2025-11-04 12:45:46 +0530

    Fix import path for UserContext in Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { motion } from "framer-motion";
-import {UserContext} from "./UserContext";
+import { UserContext } from "../context/UserContext";;
 
 const Profile = () => {
   const { user } = useContext(UserContext);
